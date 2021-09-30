e11913ce45358179a607af1b847a008277a7f966
Vincent Ains <vincent.ains4@gmail.com>
2021-09-29 20:20:12 -0400

    swappable cards

--- taTrainer/src/components/ControlPanel.tsx
+++ taTrainer/src/components/ControlPanel.tsx
@@ -1,10 +1,13 @@
 import { Button, Col } from 'react-bootstrap';
 import { Card } from '../interfaces/card';
-
+import CARDS from '../assets/cards.json'
+import { getRandomElement } from '../utilities/data';
 
 export function ControlPanel({setCard}: {setCard: (c: Card)=>void}): JSX.Element {
     return <Col>
     <h1>Control Panel</h1>
-    <Button>Swap Current Card</Button>
+    <Button onClick={
+        () => setCard(getRandomElement(CARDS as unknown as Card[]))
+    }>Swap Current Card</Button>
     </Col>
 }
