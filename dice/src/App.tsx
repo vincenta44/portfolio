4811643f631068de48e2279498f1402f7797df7e
Vincent Ains <vincent.ains4@gmail.com>
2021-09-30 20:06:55 -0400

    creasted die roller

---
+++ dice/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import { Container, Row } from 'react-bootstrap'
+
+function App() {
+  return (
+    <Container className="App">
+
+    
+    </Container>
+  );
+}
+
+export default App;
